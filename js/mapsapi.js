cba57b0605fd86574d62d6544d0ebf5f0e87f8ed
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2020-05-03 02:53:45 -0400

    changes

--- js/mapsapi.js
+++ js/mapsapi.js
@@ -9,14 +9,11 @@ function geocode() {
     })
         .then(function (response) {
             //getting longitude and latitude from the data
-            let lat = response.data.results[0].geometry.location.lat;
-            let lng = response.data.results[0].geometry.location.lng;
-            console.log(lat);
-            var a = parseFloat(lat);
-            var b = parseFloat(lng);
-            window.latitude = a;
-            window.longitude = b;
-            console.log(typeof(latitude));
+            let lat = parseFloat(response.data.results[0].geometry.location.lat);
+            let lng = parseFloat(response.data.results[0].geometry.location.lng);
+            console.log(typeof(lat));
+            window.lat = lat;
+            window.lng = lng;
         })
 };
 
@@ -24,10 +21,10 @@ function geocode() {
 // Initialize and add the map
 function initMap() {
     // The address location
-    var addressLocation = { lat: latitude1, lng:longitude }; //need to convert the stuff into this lat and lng
+    var addressLocation = { lat: this.lat, lng: this.lng }; //need to convert the stuff into this lat and lng
     // The map, centered at addressLocation
     var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 4, center: addressLocation });
+        document.getElementById('map'), { zoom: 14, center: addressLocation });
     // The marker, positioned at address location
     var marker = new google.maps.Marker({ position: addressLocation, map: map });
 }
